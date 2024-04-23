6a9f58afa9fc13ef31d545029aefbeb689a2c843
Ollie Walsh <ollie.walsh@gmail.com>
2024-04-23 09:17:59 +0100

    DNM: R1/R2

--- Firmware/LowLevel/src/main.cpp
+++ Firmware/LowLevel/src/main.cpp
@@ -57,8 +57,8 @@ SerialPIO uiSerial(PIN_UI_TX, PIN_UI_RX, 250);
 /**
  * @brief Some hardware parameters
  */
-#define VIN_R1 10000.0f
-#define VIN_R2 1000.0f
+#define VIN_R1 9980.0f
+#define VIN_R2 998.0f
 #define R_SHUNT 0.003f
 #define CURRENT_SENSE_GAIN 100.0f
 
