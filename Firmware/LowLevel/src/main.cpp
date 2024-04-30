5dc781a587c70b87475d51b2af151efeac0ea19b
Ollie Walsh <ollie.walsh@gmail.com>
2024-05-01 00:35:59 +0100

    Ensure PIN_ENABLE_CHARGE is high when undocked to allow regen

--- Firmware/LowLevel/src/main.cpp
+++ Firmware/LowLevel/src/main.cpp
@@ -597,12 +597,17 @@ bool checkShouldCharge() {
         charging_paused = true;
     }
     return !charging_paused &&
-        status_message.v_charge > 3.0f &&
         status_message.v_charge < BATT_CHARGE_OVERVOLTAGE &&
         status_message.charging_current < BATT_CHARGE_OVERCURRENT;
 }
 
 void updateChargingEnabled() {
+    // Always enable for regen when not docked
+    if (status_message.v_charge < 3.0f) {
+        digitalWrite(PIN_ENABLE_CHARGE, HIGH);
+        charging_allowed = false; // For high level status
+        return;
+    }
     if (charging_allowed) {
         if (!checkShouldCharge()) {
             digitalWrite(PIN_ENABLE_CHARGE, LOW);
